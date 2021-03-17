b74e43317d0753e579542339ec054e6fb32702d8
MX <MX@DESKTOP-PQB3BOA>
2021-03-17 22:52:31 +0300

    17 Mart Commit 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './components/product/product.component';
 import { CategoryComponent } from './components/category/category.component';
 import { NaviComponent } from './components/navi/navi.component';
 import { VatAddedPipe } from './pipes/vat-added.pipe';
-import {FormsModule} from '@angular/forms';
 import { FilterPipePipe } from './pipes/filter-pipe.pipe'
+
+import {ToastrModule } from "ngx-toastr"
+import { from } from 'rxjs';
+
+
 
 @NgModule({
   declarations: [
@@ -19,7 +26,13 @@ import { FilterPipePipe } from './pipes/filter-pipe.pipe'
     VatAddedPipe,
     FilterPipePipe,
   ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule , FormsModule],
+  imports: [BrowserModule, 
+    AppRoutingModule, 
+    HttpClientModule ,
+    BrowserAnimationsModule, 
+    FormsModule,ToastrModule.forRoot({
+    positionClass:"toast-bottom-right"
+  })],
   providers: [],
   bootstrap: [AppComponent],
 })
